793f6d2c98a0158a048002222a92224e1b249612
viliamnovicky <viliamnovicky@gmail.com>
2022-10-19 20:26:22 +0200

    fuck modal

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// Modal
-const showModal = function() {
-    const modal = document.querySelector(".modal")
-    modal.classList.remove("hidden")
-}
-
-const hideModal = function() {
-    const modal = document.querySelector(".modal")
-    modal.classList.add("hidden")
-}
-
-document.querySelector(".footer__btn").addEventListener("click", showModal)
-document.querySelector(".modal__close").addEventListener("click", hideModal)
-
 // Hamburger
 const hideNavbar = function(e) {
     e.preventDefault
